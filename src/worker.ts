f52c0391c4a55b4ab5658e7846ac7559b41e0861
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2025-12-12 12:39:01 +0000

    incredibly basic "playground"

--- src/worker.ts
+++ src/worker.ts
@@ -33,21 +33,67 @@ export default {
       });
     }
 
-    let tsCode = "";
+    let code = "";
 
     if (request.method === "GET") {
-      tsCode = new URL(request.url).searchParams.get("code") || "";
+      const {searchParams} = new URL(request.url);
+      code = searchParams.get("code") || "";
     } else if (request.method === "POST") {
-      tsCode = await request.text();
+      const text = await request.text();
+      if (text.startsWith("{")) {
+        code = JSON.parse(text).code;
+      } else {
+        code = text;
+      }
     } else {
       return jsonResponse({success: false, error: "Method not allowed"}, 405);
     }
 
-    if (!tsCode.trim()) {
+    code = code.trim();
+
+    if (!code && request.method === 'GET') {
+      const initialCode = [
+        "interface User {",
+        "  name: string;",
+        "  age: number;",
+        "}",
+        "",
+        "function greet(user: User): string {",
+        "  return `Hello ${user.name}, you are ${user.age} years old.`;",
+        "}",
+        "",
+        "async function codemode() {",
+        "  return greet({name: 'John', age: 40});",
+        "}",
+      ].join('\n')
+      const html = `
+        <main style="display: flex; flex-direction: column; max-width: 600px; margin: 12px;">
+          <h2>write some typescript code below</h2>
+          <p>the code will be executed in a dynamic worker. you must define a function called <code>codemode</code>.</p>
+          <textarea
+            id="tscode"
+            rows="20"
+            cols="80"
+          >${initialCode}</textarea>
+          <button
+            id="run"
+            style="display: block; font-size: xx-large;"
+            onclick="document.getElementById('result').src = '/?code=' + encodeURIComponent(document.getElementById('tscode').value);"
+          >run</button>
+          <iframe id="result" style="width: 100%; height: 500px; border: 1px solid #ccc;"></iframe>
+        </main>
+      `
+      return new Response(html, {
+        status: 200,
+        headers: {"Content-Type": "text/html", "Access-Control-Allow-Origin": "*"},
+      });
+    }
+
+    if (!code.trim()) {
       return jsonResponse({success: false, error: "Request body is empty. Please provide TypeScript code."}, 400);
     }
 
-    const compileResult = compileCode(tsCode);
+    const compileResult = compileCode(code);
 
     if (!compileResult.success) {
       return jsonResponse<ErrorResponse>(
